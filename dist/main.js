a12c93636815d3e8539ed678db23d965457fd56a
MauMuller <mauriciomuller@gmail.com>
2022-08-20 13:26:42 -0300

    Atualização dos arquivos do WEBPACK: packeage.json e ./src

--- dist/main.js
+++ dist/main.js
@@ -13,106 +13,20 @@
 /*!**********************!*\
   !*** ./src/index.js ***!
   \**********************/
-/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+/***/ (() => {
 
-"use strict";
-eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _models_planet__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./models/planet */ \"./src/models/planet.js\");\n/* harmony import */ var _models_planet__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_models_planet__WEBPACK_IMPORTED_MODULE_0__);\n/* harmony import */ var _models_solar_system__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./models/solar_system */ \"./src/models/solar_system.js\");\n/* harmony import */ var _models_solar_system__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(_models_solar_system__WEBPACK_IMPORTED_MODULE_1__);\n\n\nvar planet = new (_models_planet__WEBPACK_IMPORTED_MODULE_0___default())(\"Terra\", \"342323m²\");\nalert(JSON.stringify((_models_solar_system__WEBPACK_IMPORTED_MODULE_1___default())));\nalert(JSON.stringify(planet));\nplanet.rotate();\nconsole.log(planet);\nconsole.log((_models_solar_system__WEBPACK_IMPORTED_MODULE_1___default()));\n\n//# sourceURL=webpack://ex4/./src/index.js?");
-
-/***/ }),
-
-/***/ "./src/models/planet.js":
-/*!******************************!*\
-  !*** ./src/models/planet.js ***!
-  \******************************/
-/***/ ((module) => {
-
-eval("function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError(\"Cannot call a class as a function\"); } }\n\nfunction _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if (\"value\" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }\n\nfunction _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); Object.defineProperty(Constructor, \"prototype\", { writable: false }); return Constructor; }\n\nvar Planet = /*#__PURE__*/function () {\n  function Planet(name, surface) {\n    _classCallCheck(this, Planet);\n\n    this.name = name, this.surface = surface;\n  }\n\n  _createClass(Planet, [{\n    key: \"rotate\",\n    value: function rotate() {\n      console.log(\"roda roda jequiti mlk!\");\n    }\n  }]);\n\n  return Planet;\n}();\n\nmodule.exports = Planet;\n\n//# sourceURL=webpack://ex4/./src/models/planet.js?");
-
-/***/ }),
-
-/***/ "./src/models/solar_system.js":
-/*!************************************!*\
-  !*** ./src/models/solar_system.js ***!
-  \************************************/
-/***/ ((module) => {
-
-eval("module.exports = {\n  planets: [],\n  central_star: \"Sol\"\n};\n\n//# sourceURL=webpack://ex4/./src/models/solar_system.js?");
+eval("console.log(\"a\");\n\n//# sourceURL=webpack://ex4/./src/index.js?");
 
 /***/ })
 
 /******/ 	});
 /************************************************************************/
-/******/ 	// The module cache
-/******/ 	var __webpack_module_cache__ = {};
-/******/ 	
-/******/ 	// The require function
-/******/ 	function __webpack_require__(moduleId) {
-/******/ 		// Check if module is in cache
-/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
-/******/ 		if (cachedModule !== undefined) {
-/******/ 			return cachedModule.exports;
-/******/ 		}
-/******/ 		// Create a new module (and put it into the cache)
-/******/ 		var module = __webpack_module_cache__[moduleId] = {
-/******/ 			// no module.id needed
-/******/ 			// no module.loaded needed
-/******/ 			exports: {}
-/******/ 		};
-/******/ 	
-/******/ 		// Execute the module function
-/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
-/******/ 	
-/******/ 		// Return the exports of the module
-/******/ 		return module.exports;
-/******/ 	}
-/******/ 	
-/************************************************************************/
-/******/ 	/* webpack/runtime/compat get default export */
-/******/ 	(() => {
-/******/ 		// getDefaultExport function for compatibility with non-harmony modules
-/******/ 		__webpack_require__.n = (module) => {
-/******/ 			var getter = module && module.__esModule ?
-/******/ 				() => (module['default']) :
-/******/ 				() => (module);
-/******/ 			__webpack_require__.d(getter, { a: getter });
-/******/ 			return getter;
-/******/ 		};
-/******/ 	})();
-/******/ 	
-/******/ 	/* webpack/runtime/define property getters */
-/******/ 	(() => {
-/******/ 		// define getter functions for harmony exports
-/******/ 		__webpack_require__.d = (exports, definition) => {
-/******/ 			for(var key in definition) {
-/******/ 				if(__webpack_require__.o(definition, key) && !__webpack_require__.o(exports, key)) {
-/******/ 					Object.defineProperty(exports, key, { enumerable: true, get: definition[key] });
-/******/ 				}
-/******/ 			}
-/******/ 		};
-/******/ 	})();
-/******/ 	
-/******/ 	/* webpack/runtime/hasOwnProperty shorthand */
-/******/ 	(() => {
-/******/ 		__webpack_require__.o = (obj, prop) => (Object.prototype.hasOwnProperty.call(obj, prop))
-/******/ 	})();
-/******/ 	
-/******/ 	/* webpack/runtime/make namespace object */
-/******/ 	(() => {
-/******/ 		// define __esModule on exports
-/******/ 		__webpack_require__.r = (exports) => {
-/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
-/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
-/******/ 			}
-/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
-/******/ 		};
-/******/ 	})();
-/******/ 	
-/************************************************************************/
 /******/ 	
 /******/ 	// startup
 /******/ 	// Load entry module and return exports
 /******/ 	// This entry module can't be inlined because the eval devtool is used.
-/******/ 	var __webpack_exports__ = __webpack_require__("./src/index.js");
+/******/ 	var __webpack_exports__ = {};
+/******/ 	__webpack_modules__["./src/index.js"]();
 /******/ 	
 /******/ })()
 ;
